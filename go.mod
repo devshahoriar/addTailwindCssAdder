1b8b4cf7ed4641b317ef09a48817dda1163cde55
devshahoriar <shuvo1556@gmail.com>
2024-01-14 10:57:05 +0600

    can detect your project

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/devshahoriar/addTailwindCssAdder
 
 go 1.21.5
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/gookit/color/colorp"
+	"github.com/manifoldco/promptui"
+)
 
 func main() {
-	fmt.Println("Working")
+	if _, err := os.Stat("./vite.config.js"); err == nil {
+		if _, err := os.Stat("./package.json"); err == nil {
+
+			prompt := promptui.Select{
+				Label: "Select your package manager",
+				Items: []string{"npm", "yarn", "pnpm", "bun"},
+			}
+
+			_, selectedPackge, err := prompt.Run()
+
+			if err != nil {
+				fmt.Printf("Prompt failed %v\n", err)
+				return
+			}
+
+			app := "node"
+
+			arg0 := "-v"
+
+			cmd := exec.Command(app, arg0)
+			stdout, err := cmd.Output()
+
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			colorp.Bluef("your are using node->%s and %s.\n", strings.TrimSpace(string(stdout)), selectedPackge)
+			colorp.Grayln("Installing packages..")
+
+		} else {
+			colorp.Redp("Couldn't find package.json.")
+		}
+	} else {
+		colorp.Redp("This is not vite project!")
+	}
+
 }
